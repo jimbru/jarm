088b1dd57e50f956437f3f96ee00eebcd244bda3
Jim Brusstar <jim.brusstar@gmail.com>
2015-06-02 19:00:07 -0700

    Clarify ambiguous Pomegranate dependency

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.cemerick/pomegranate "0.3.0"]
+  :dependencies [[com.cemerick/pomegranate "0.3.0" :exclusions [org.codehaus.plexus/plexus-utils]]
                  [ext "0.1.2"]
                  [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.1"]]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [org.codehaus.plexus/plexus-utils "2.0.6"]]  ;; Pomegranate dependency
   :main ^:skip-aot jarm.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
